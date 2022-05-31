426bbc0ec77a68fad56250bfef56ef0d24b50766
azusameow <106598538+azusameow@users.noreply.github.com>
2022-05-31 20:58:34 +0800

    Update xzx.java

--- xzx.java
+++ xzx.java
@@ -1,3 +1,18 @@
+
+package com.example.twapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
 public class MainActivity extends AppCompatActivity {
     private Button btn_WristbandLock;
     private Button btn_CurrentLocation;
