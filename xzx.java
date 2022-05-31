925ca986f95ff139b4901761d917ea3f74f8184f
azusameow <106598538+azusameow@users.noreply.github.com>
2022-05-31 20:45:57 +0800

    Update xzx.java

--- xzx.java
+++ xzx.java
@@ -1,18 +1,3 @@
-package com.example.twapp;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import android.util.Log;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.messaging.FirebaseMessaging;
-
-
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = FCMService.TAG;
@@ -42,10 +27,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        
+    
         //............Do something
-
     }
-
-
 }
